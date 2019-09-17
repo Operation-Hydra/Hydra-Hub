f09e995cca67d429c8fab3d148bdfbcef43f0758
Christian Aichner <contact@aichner-christian.com>
2019-09-17 23:19:59 +0200

    Add loader
    A basic loader has been added to the dashboard.

--- src/components/pages/Dashboard/index.jsx
+++ src/components/pages/Dashboard/index.jsx
@@ -10,6 +10,8 @@ import { Link, Redirect } from 'react-router-dom';
 // "Material Design for Bootstrap" is a great UI design framework
 import { 
     MDBContainer,
+    MDBEdgeHeader,
+    MDBFreeBird,
     MDBRow,
     MDBCol,
     MDBNavLink,
@@ -17,6 +19,7 @@ import {
     MDBNavItem,
     MDBTabContent,
     MDBNav,
+    MDBAlert,
     MDBBadge,
     MDBBtn,
     MDBPopover,
@@ -27,6 +30,8 @@ import {
     MDBModalHeader,
     MDBCard,
     MDBCardBody,
+    MDBSpinner,
+    MDBProgress,
 } from 'mdbreact';
 
 //> Redux
@@ -72,89 +77,118 @@ class Dashboard extends React.Component{
 
         console.log(profile);
 
-        return(
-            <div id="dashboard">
-                <MDBContainer className="py-5 ubuntu">
-                    <MDBCard>
-                        <MDBCardBody className="text-center">
-                        <img className="d-block m-auto img-fluid" src={EagleLogo} alt="Hydra Logo"/>
-                            { profile.sex === "m" ? (
-                                <h3>Welcome to work, Mr. { profile.last_name }</h3>
-                            ) : (
-                                <>
-                                { profile.relationship ? (
-                                    <h3>Welcome to work, Mrs. { profile.last_name }</h3> 
+        if(profile.isLoaded){
+            return(
+                <div id="dashboard">
+                    <MDBContainer className="py-5 ubuntu">
+                        <MDBCard>
+                            <MDBCardBody className="text-center">
+                            <img className="d-block m-auto img-fluid" src={EagleLogo} alt="Hydra Logo"/>
+                                { profile.sex === "m" ? (
+                                    <h3>Welcome to work, Mr. { profile.last_name }</h3>
                                 ) : (
-                                    <h3>Welcome to work, Ms. { profile.last_name }</h3> 
+                                    <>
+                                    { profile.relationship ? (
+                                        <h3>Welcome to work, Mrs. { profile.last_name }</h3> 
+                                    ) : (
+                                        <h3>Welcome to work, Ms. { profile.last_name }</h3> 
+                                    )}
+                                    </>
                                 )}
-                                </>
-                            )}
-                            <p>Your work is important. It helps making the world a better and safer place for everyone.</p>
-                            <MDBRow>
-                                <MDBCol md="6" className="text-left">
-                                    <Link
-                                    to="/search"
-                                    >
-                                        <MDBBtn 
-                                        color="elegant"
+                                <p>Your work is important. It helps making the world a better and safer place for everyone.</p>
+                                <MDBRow>
+                                    <MDBCol md="6" className="text-left">
+                                        <Link
+                                        to="/search"
                                         >
-                                        <MDBIcon icon="terminal" className="pr-2" />
-                                        Terminal
+                                            <MDBBtn 
+                                            color="elegant"
+                                            >
+                                            <MDBIcon icon="terminal" className="pr-2" />
+                                            Terminal
+                                            </MDBBtn>
+                                        </Link>
+                                    </MDBCol>
+                                    <MDBCol md="6" className="text-right">
+                                        <MDBBtn 
+                                        color="danger"
+                                        onClick={this.props.signOut}
+                                        >
+                                        Sign out
+                                        <MDBIcon icon="sign-out-alt" className="pl-2" />
                                         </MDBBtn>
-                                    </Link>
+                                    </MDBCol>
+                                </MDBRow>
+                                
+                            </MDBCardBody>
+                        </MDBCard>
+                        { profile.level > 8 &&
+                            <MDBRow className="flex-center">
+                                <MDBCol md="6">
+                                    <MDBCard className="my-5 text-center">
+                                        <MDBCardBody>
+                                            <h4 className="font-weight-bold">Project I.N.S.I.G.H.T.</h4>
+                                            <p>View live targets of Project I.N.S.I.G.H.T.</p>
+                                            <WorldMap/>
+                                            {profile.level === 10 &&
+                                                <Link
+                                                to="/zoola/list"
+                                                >
+                                                    <MDBBtn 
+                                                    size="md" 
+                                                    color="elegant"
+                                                    outline
+                                                    >
+                                                    <MDBIcon icon="list" className="pr-2" />
+                                                    Target List
+                                                    </MDBBtn>
+                                                </Link>
+                                            }
+                                        </MDBCardBody>
+                                    </MDBCard>
                                 </MDBCol>
-                                <MDBCol md="6" className="text-right">
-                                    <MDBBtn 
-                                    color="danger"
-                                    onClick={this.props.signOut}
-                                    >
-                                    Sign out
-                                    <MDBIcon icon="sign-out-alt" className="pl-2" />
-                                    </MDBBtn>
+                                <MDBCol md="6">
+                                    <MDBCard className="my-5 text-center">
+                                        <MDBCardBody>
+                                            <h4 className="font-weight-bold">Active agents in Austria</h4>
+                                            <AustriaMap/>
+                                        </MDBCardBody>
+                                    </MDBCard>
                                 </MDBCol>
                             </MDBRow>
-                            
-                        </MDBCardBody>
-                    </MDBCard>
-                    { profile.level > 8 &&
-                        <MDBRow className="flex-center">
-                            <MDBCol md="6">
-                                <MDBCard className="my-5 text-center">
-                                    <MDBCardBody>
-                                        <h4 className="font-weight-bold">Project I.N.S.I.G.H.T.</h4>
-                                        <p>View live targets of Project I.N.S.I.G.H.T.</p>
-                                        <WorldMap/>
-                                        {profile.level === 10 &&
-                                            <Link
-                                            to="/zoola/list"
-                                            >
-                                                <MDBBtn 
-                                                size="md" 
-                                                color="elegant"
-                                                outline
-                                                >
-                                                <MDBIcon icon="list" className="pr-2" />
-                                                Target List
-                                                </MDBBtn>
-                                            </Link>
-                                        }
-                                    </MDBCardBody>
-                                </MDBCard>
-                            </MDBCol>
-                            <MDBCol md="6">
-                                <MDBCard className="my-5 text-center">
-                                    <MDBCardBody>
-                                        <h4 className="font-weight-bold">Active agents in Austria</h4>
-                                        <AustriaMap/>
-                                    </MDBCardBody>
-                                </MDBCard>
+                        }
+                    </MDBContainer>
+                    <div className="intro"></div>
+                </div>
+            );
+        } else {
+            return (
+                <div id="dashboard">
+                    <MDBEdgeHeader color="green lighten-3" />
+                    <MDBFreeBird>
+                        <MDBRow>
+                            <MDBCol
+                            md="10"
+                            className="mx-auto float-none white z-depth-1 py-2 px-2"
+                            >
+                                <MDBCardBody className="text-center">
+                                <MDBProgress material preloader />
+                                    <MDBAlert color="success">
+                                    <h2 
+                                    className="font-weight-bold"
+                                    >
+                                    Authorized.
+                                    </h2>
+                                    <p className="lead">Your dashboard is being loaded.</p>
+                                    </MDBAlert>
+                                </MDBCardBody>
                             </MDBCol>
                         </MDBRow>
-                    }
-                </MDBContainer>
-                <div className="intro"></div>
-            </div>
-        );
+                    </MDBFreeBird>
+                </div>
+            )
+        }
+        
     }
 }
 
